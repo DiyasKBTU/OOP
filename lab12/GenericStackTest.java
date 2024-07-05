7248cd0c3f8b034a7f37afdb632b3e05a29cab23
DiyasKBTU <157691950+DiyasKBTU@users.noreply.github.com>
2024-07-05 22:49:00 +0500

    Update GenericStackTest.java

--- lab12/GenericStackTest.java
+++ lab12/GenericStackTest.java
@@ -3,7 +3,7 @@ package lab12;
 public class GenericStackTest {
 	public static void main(String[] args) {
 		GenericStack<Integer> st = new GenericStack<>();
-		for(int i = 2; i <= 50; i++) {
+		for(int i = 50; i >=2; i--) {
 			boolean b = true;
 			for(int j = 2; j < i; j++) {
 				if(i % j == 0) {
