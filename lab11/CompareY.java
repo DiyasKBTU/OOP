60297fbe172fa7d2a92f2e16e355129be27abeda
DiyasKBTU <157691950+DiyasKBTU@users.noreply.github.com>
2024-07-05 23:56:15 +0500

    Update CompareY.java

--- lab11/CompareY.java
+++ lab11/CompareY.java
@@ -4,9 +4,27 @@ import java.util.Comparator;
 
 class CompareY implements Comparator<Point> {
     public int compare(Point p1, Point p2) {
-        if (p1.getY() == p2.getY()) {
-            return Integer.compare(p1.getX(), p2.getX());
-        }
-        return Integer.compare(p1.getY(), p2.getY());
+        if(p1.getY() == p2.getY()) {
+    		if(p1.getX() > p2.getX()) {
+    			return 1;
+    		}
+    		else if(p1.getX() > p2.getX()) {
+    			return 0;
+    		}
+    		else {
+    			return -1;
+    		}
+    	}
+    	else {
+    		if(p1.getY() > p2.getY()) {
+    			return 1;
+    		}
+    		else if(p1.getY() == p2.getY()) {
+    			return 0;
+    		}
+    		else {
+    			return -1;
+    		}
+    	}
     }
 }
