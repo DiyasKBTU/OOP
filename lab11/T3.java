57c316d205ac1703ea8ab89a60eacc56b106d06d
DiyasKBTU <157691950+DiyasKBTU@users.noreply.github.com>
2024-07-05 23:13:05 +0500

    Update T3.java

--- lab11/T3.java
+++ lab11/T3.java
@@ -1,11 +1,12 @@
 package lab11;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class T3 {
 	public static <E extends Comparable<E>> E max(ArrayList<E> list) {
 		if (list == null || list.isEmpty()) {
-            return null;
+            		return null;
         }
         
         E max = list.get(0);
@@ -14,11 +15,14 @@ public class T3 {
                 max = element;
             }
         }
-        return max;
+             return max;
 	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ArrayList<Integer> arr = new ArrayList<>(List.of(5, 2, 1, 3, 6, 4));
+		
+		int max = max(arr);
+		System.out.println(max);
 
 	}
 
